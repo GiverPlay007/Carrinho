69382e4c20dacf39967ab99d12bfb8156e0bb172
GiverPlay007 <play.giver.007@gmail.com>
2022-05-05 14:22:04 -0300

    H-Bridge

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Carrinho
-:bulb: Futuro projeto com Arduino e ESP8266
+## Pinagem
+Importante: Foi usado um conversor de nível lógico para a comunicação entre o módulo Ponte-H.
+
+| ESP32 | Circuito       |
+| :---  | :---           |
+| G25   | L298N Enable A |
+| G26   | L298N Input 1  |
+| G26   | L298N Input 2  | 
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,40 @@
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  pinMode(GPIO_NUM_25, OUTPUT);
+  pinMode(GPIO_NUM_26, OUTPUT);
+  pinMode(GPIO_NUM_27, OUTPUT);
+
+  ledcSetup(0, 30000, 8);
+  ledcAttachPin(GPIO_NUM_25, 0);
+
+  ledcWrite(0, 254);
 }
 
 void loop()
 {
-  Serial.println("Hello");
-  delay(500);
+  if(Serial.available())
+  {
+    int input = Serial.parseInt();
+
+    switch(input) {
+      case 1:
+        digitalWrite(GPIO_NUM_26, HIGH);
+        digitalWrite(GPIO_NUM_27, LOW);
+        Serial.println("C 1");
+        break;
+      case 2:
+        digitalWrite(GPIO_NUM_27, HIGH);
+        digitalWrite(GPIO_NUM_26, LOW);
+        Serial.println("C 2");
+        break;
+      case 3:
+        digitalWrite(GPIO_NUM_26, LOW);
+        digitalWrite(GPIO_NUM_27, LOW);
+        Serial.println("C 3");
+        break;
+    }
+  }
 }
