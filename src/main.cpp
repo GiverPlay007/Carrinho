f3e55e3421329df7d1d8c869332a1f0c156ee0a4
GiverPlay007 <play.giver.007@gmail.com>
2022-05-05 15:24:18 -0300

    Direction

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+#define DIRECTION_CENTER 94
+#define DIRECTION_LEFT 0
+#define DIRECTION_RIGHT 180
+
+Servo direction;
+int angle = DIRECTION_CENTER;
 
 void setup()
 {
   Serial.begin(115200);
 
+  pinMode(GPIO_NUM_14, OUTPUT);
   pinMode(GPIO_NUM_25, OUTPUT);
   pinMode(GPIO_NUM_26, OUTPUT);
   pinMode(GPIO_NUM_27, OUTPUT);
 
-  ledcSetup(0, 30000, 8);
-  ledcAttachPin(GPIO_NUM_25, 0);
+  ledcSetup(10, 30000, 8);
+  ledcAttachPin(GPIO_NUM_25, 10);
 
-  ledcWrite(0, 254);
+  ledcWrite(10, 254);
+
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+
+  direction.setPeriodHertz(50);
+  direction.attach(GPIO_NUM_14, 500, 2400);
 }
 
 void loop()
@@ -22,20 +39,34 @@ void loop()
 
     switch(input) {
       case 1:
-        digitalWrite(GPIO_NUM_26, HIGH);
+        digitalWrite(GPIO_NUM_26, LOW);
         digitalWrite(GPIO_NUM_27, LOW);
-        Serial.println("C 1");
+        Serial.println("STOP");
         break;
       case 2:
-        digitalWrite(GPIO_NUM_27, HIGH);
-        digitalWrite(GPIO_NUM_26, LOW);
-        Serial.println("C 2");
+        digitalWrite(GPIO_NUM_26, HIGH);
+        digitalWrite(GPIO_NUM_27, LOW);
+        Serial.println("FORWARD");
         break;
       case 3:
         digitalWrite(GPIO_NUM_26, LOW);
-        digitalWrite(GPIO_NUM_27, LOW);
-        Serial.println("C 3");
+        digitalWrite(GPIO_NUM_27, HIGH);
+        Serial.println("BACKWARD");
+        break;
+      case 4:
+        angle = DIRECTION_CENTER;
+        Serial.println("CENTER");
+        break;
+      case 5:
+        angle = DIRECTION_RIGHT;
+        Serial.println("RIGHT");
+        break;
+      case 6:
+        angle = DIRECTION_LEFT;
+        Serial.println("LEFT");
         break;
     }
   }
+
+  direction.write(angle);
 }
